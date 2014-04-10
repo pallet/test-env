6c350e5a323884308d8c47d2682277db5346527c
Hugo Duncan <hugo@hugoduncan.org>
2014-04-10 14:55:56 -0400

    Use :subprocess false for lein-modules
    This avoids issues with the lein executable name hard coded in
    lein-modules, which doesn't match the `lein2` name used on travis.

--- lein-test-env/project.clj
+++ lein-test-env/project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
-  :dependencies [[configleaf "0.4.6"]])
+  :dependencies [[configleaf "0.4.6"]]
+  :exclusions [org.clojure/clojure])
--- test-env/profiles.clj
+++ test-env/profiles.clj
@@ -1,7 +1,9 @@
 {:dev {:dependencies [[ch.qos.logback/logback-classic "1.0.9"]]
        :plugins [[lein-modules "0.2.1"]]
        :modules {:parent "../project.clj"}}
- :provided {:dependencies [[com.palletops/pallet "0.8.0-RC.9"]]}
+ :provided {:dependencies [[com.palletops/pallet "0.8.0-RC.9"
+                            :exclusions [org.clojure/clojure]]
+                           [org.clojure/clojure "1.5.1"]]}
  :no-checkouts {:checkout-deps-shares ^{:replace true} []}
  :release {:set-version {:updates [{:path "README.md"
                                     :no-snapshot true}]}}}
