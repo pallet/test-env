7761780618d06691e98bd1d9d723264f14195d91
Hugo Duncan <hugo@palletops.com>
2014-04-11 18:12:04 +0000

    Updated version for next release cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.palletops/pallet-test-env-parent "0.1.3"
+(defproject com.palletops/pallet-test-env-parent "0.1.4-SNAPSHOT"
   :packaging "pom"
   :aliases {"jar" ["do"] ; do is a no-op when called with no arguments
             "install" ["do"]
