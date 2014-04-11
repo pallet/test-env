8920ccb967687da4d7d4afb7a678a669fdf34dc3
Hugo Duncan <hugo@hugoduncan.org>
2014-04-11 13:56:26 -0400

    Upgrade to lein-modules 0.2.2

--- lein-test-env/profiles.clj
+++ lein-test-env/profiles.clj
@@ -1,6 +1,5 @@
-{:dev {:modules {:parent "../project.clj"
-                 :subprocess false}
-       :plugins [[lein-modules "0.2.1"]]}
+{:dev {:modules {:subprocess false}
+       :plugins [[lein-modules "0.2.2"]]}
  :no-checkouts {:checkout-deps-shares ^{:replace true} []}
  :release {:set-version {:updates [{:path "README.md"
                                     :no-snapshot true}]}}}
--- test-env/profiles.clj
+++ test-env/profiles.clj
@@ -1,7 +1,6 @@
 {:dev {:dependencies [[ch.qos.logback/logback-classic "1.0.9"]]
-       :plugins [[lein-modules "0.2.1"]]
-       :modules {:parent "../project.clj"
-                 :subprocess false}}
+       :plugins [[lein-modules "0.2.2"]]
+       :modules {:subprocess false}}
  :provided {:dependencies [[com.palletops/pallet "0.8.0-RC.9"
                             :exclusions [org.clojure/clojure]]
                            [org.clojure/clojure "1.5.1"]]}
