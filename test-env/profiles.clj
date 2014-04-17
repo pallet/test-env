6741760779ee02710c53bbdb14095f9c64ed137c
Hugo Duncan <hugo@hugoduncan.org>
2014-04-17 14:32:48 -0400

    Fix provided scope for lein 2.3.4

--- test-env/profiles.clj
+++ test-env/profiles.clj
@@ -2,7 +2,8 @@
        :plugins [[lein-modules "0.2.4"]]
        :modules {:subprocess false}}
  :provided {:dependencies [[com.palletops/pallet "0.8.0-RC.9"
-                            :exclusions [org.clojure/clojure]]]}
+                            :exclusions [org.clojure/clojure]]
+                           [org.clojure/clojure "1.5.1"]]}
  :no-checkouts {:checkout-deps-shares ^{:replace true} []}
  :release {:set-version {:updates [{:path "README.md"
                                     :no-snapshot true}]}}}
